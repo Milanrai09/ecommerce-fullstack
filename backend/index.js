ce0238ef6b471a50054346461543a6ee8e43474c
Milan Rai <111414440+Milanrai09@users.noreply.github.com>
2024-08-28 18:58:56 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -20,23 +20,7 @@ app.use(cookieParser());
 app.use(bodyParser.json()); // Use only for JSON parsing
 app.use(bodyParser.urlencoded({ extended: true })); // Only if required for form data
 
-const allowCors = fn => async (req, res, next) => {
-  res.setHeader('Access-Control-Allow-Credentials', 'true');
-  res.setHeader('Access-Control-Allow-Origin', 'https://ecommerce-fullstack-frontend-theta.vercel.app');
-  res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT');
-  res.setHeader(
-    'Access-Control-Allow-Headers',
-    'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
-  );
-  if (req.method === 'OPTIONS') {
-    res.status(200).end();
-    return;
-  }
-  return fn(req, res, next);
-};
 
-// Apply custom CORS to all routes
-app.use(allowCors((_req, _res, next) => next()));
 
 
 app.use(cookieParser());
