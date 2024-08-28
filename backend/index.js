e117cdbf8b83a3249466ff25bc83714907dc1893
Milan Rai <111414440+Milanrai09@users.noreply.github.com>
2024-08-28 19:03:47 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -2,7 +2,6 @@ require("dotenv").config();
 const express = require('express');
 const app = express();
 const port = process.env.PORT
-// const orderRouter = require('./routes/orderRoute');
 const userRouter = require('./routes/userRoute');
 const productRouter = require('./routes/productRoute');
 const adminRouter = require('./routes/adminRoute');
@@ -20,8 +19,6 @@ app.use(bodyParser.json());
 
 
 
-// app.use('/api/order', orderRouter);
-// app.use('/api/product', productRouter);
 app.use('/api/users' , userRouter);
 app.use('/api/product',productRouter)
 app.use('/api/admin',adminRouter)
