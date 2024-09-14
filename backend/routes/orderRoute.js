bac1b3ec34c9bf664894f7e8081b3cbe0b1277ff
Milan Rai <111414440+Milanrai09@users.noreply.github.com>
2024-09-14 19:38:16 +0530

    Update orderRoute.js

--- backend/routes/orderRoute.js
+++ backend/routes/orderRoute.js
@@ -6,6 +6,5 @@ const router = express.Router();
 // router.post('/placeOrder',createOrder);
 router.get('/:id',getUserOrders);
 router.post('/createorders/:userid',savedOrder);
-router.put('/update',updateOrder)
 
 module.exports = router;
