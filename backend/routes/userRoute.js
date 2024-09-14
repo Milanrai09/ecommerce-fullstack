11a126ebf378b9af7c903e2ffcb39684ab878244
Milan Rai <111414440+Milanrai09@users.noreply.github.com>
2024-09-14 19:25:51 +0530

    Update userRoute.js

--- backend/routes/userRoute.js
+++ backend/routes/userRoute.js
@@ -5,15 +5,11 @@ const { cancelOrder } = require('../controllers/adminController');
 const { verifyToken } = require('../middleware/userMiddleware');
 const router = express.Router();
 
-router.get('getUserById/:id', getUser);
 router.post('/register', registerUser);
 router.post('/login', loginUser);
 router.get('/cart',verifyToken,getCart);
 router.get('/',getAllUser)
-router.post('/logout',userLogout);
 router.post('/addtocart',verifyToken,createCarts)
-router.put('/updateCart',updateCart)
-router.put('/order/cancel/:id',cancelOrder)
 module.exports = router;
 
 
