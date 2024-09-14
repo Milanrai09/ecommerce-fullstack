4976951cda67735388f365d406a12aa3f7c14659
Milan Rai <111414440+Milanrai09@users.noreply.github.com>
2024-09-14 19:37:02 +0530

    Update adminRoute.js

--- backend/routes/adminRoute.js
+++ backend/routes/adminRoute.js
@@ -5,16 +5,11 @@ const { verifyToken, verifyAdmin } = require('../middleware/userMiddleware');
 const router = express.Router();
 
 router.get('/users',getAllUser);
-router.post('/addProduct',addProduct);
 router.delete('/removeProduct/:id',removeProduct);
 router.put('/promote/:id',makeAdmin);
-router.put('/demote',demoteAdmin);
 router.get('/order',verifyToken,getAllOrders);
-router.put('/update',updateUser);
 router.put('/cancel/:id',cancelOrder)
 router.get('/getProduct',getProduct)
-router.delete('/removeUser/:id',removeUser)
-router.put('/updateUser/:id',updateUser)
 router.put('/updateOrder',verifyAdmin,AdminupdateOrder)
 router.put('/products/updateProduct',verifyAdmin,updateProduct)
 
